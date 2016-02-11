406f8ff4e120c22510f01cb70d7d266c8e771b1b
Ryon <league@Mac-mini-3.local>
2016-02-10 17:32:07 -0800

    DabBot robot with random colored spiral :)

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,19 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
+Robot dabBot=new Robot();
 	
-		
-		
+dabBot.moveTo(300, 400);
+dabBot.setWindowColor(164,249,232);
+dabBot.setPenColor(Color.white);
+dabBot.penDown();
+for (int i = 0; i < 4; i++) {
+	dabBot.move(200);
+	dabBot.turn(90);
+}
+for (int i = 0; i < 4; i++) {
+	dabBot.move(100)
+	dabBot.turn(90)
+}
 	}
 }
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -6,22 +6,32 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
+		Robot dabBot=new Robot();
 		
 		// 5. Set your robot's pen to the down position
+		dabBot.penDown();
 
 		// 3. Set the robot to go at max speed (10)
+		dabBot.setSpeed(10);
 
 		// 4. Do the following (steps 7-9) 75 times
+		for (int i = 0; i < 2000; i++) {
+			
 
 			// 7. Change the pen color to random
+		dabBot.setRandomPenColor();
 	
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
+		dabBot.move(5*i);
 	
 			// 2. Turn the robot 1/3 of 360 degrees to the right
+		dabBot.turn(360/7);
 	
 			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
-	
-			// 9. Set the pen width to i
 
+			// 9. Set the pen width to i
+		dabBot.setPenWidth(i);
+
+		}
 	}
 }
